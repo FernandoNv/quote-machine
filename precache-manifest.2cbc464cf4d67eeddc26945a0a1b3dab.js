01fc46a4e84fe4ba0e0a139b2bd2fecafd0c729b
FernandoNv <fernandonvieira47@hotmail.com>
2020-08-15 21:41:36 -0300

    Updates

---
+++ precache-manifest.2cbc464cf4d67eeddc26945a0a1b3dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f41ec85a7c89467a521751a75b9df8c",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "e946f8e91fc5f29dfcac",
+    "url": "/quote-machine/static/css/main.8ee84601.chunk.css"
+  },
+  {
+    "revision": "f30012cb2d99ac1e8427",
+    "url": "/quote-machine/static/js/2.82327c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.82327c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e946f8e91fc5f29dfcac",
+    "url": "/quote-machine/static/js/main.d702dcf3.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  }
+]);
